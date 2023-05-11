e954fea24bb23817a6a0a5cd598915e817749ee5
junsu0573 <junsu0573@naver.com>
2023-05-12 04:30:54 +0900

    make new project secondapp

---
+++ secondapp/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:secondapp/second.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page 1'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("open page2"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ secondapp/lib/second.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('page 2'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
