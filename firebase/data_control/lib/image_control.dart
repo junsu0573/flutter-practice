77dfbf1d83e05183b2fd026462bca3c5960f0895
junsu0573 <junsu0573@naver.com>
2023-06-14 18:15:31 +0900

    add camera control comments on data control app

--- firebase/data_control/lib/image_control.dart
+++ firebase/data_control/lib/image_control.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+
+// image_picker package를 사용하여 카메라 컨트롤
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
@@ -13,19 +15,24 @@ class ImageControl extends StatefulWidget {
 class _ImageControlState extends State<ImageControl> {
   File? pickedImage;
 
+  // 이미지 촬영을 위한 메소드이다.
+  // ImagePicker()로 해당 메소드를 호출하고,
+  // 카메라를 source로 하여 촬영한다.
   void _pickImage() async {
     final imagePicker = ImagePicker();
     final pickedImageFile = await imagePicker.pickImage(
       source: ImageSource.camera,
       imageQuality: 100,
-      maxHeight: 30,
     );
 
     setState(() {
       if (pickedImageFile != null) {
+        // 만약 촬영한 이미지 파일이 존재한다면 해당 코드를 실행한다.
+        // pickedImage에 촬영한 이미지를 달아놓는다.
         pickedImage = File(pickedImageFile.path);
       }
     });
+    // Firebase Storage의 저장 위치를 가리키는 변수를 생성하고 putFile()을 통해 촬영한 이미지를 해당 위치에 저장한다.
     final refImage =
         FirebaseStorage.instance.ref().child('picked_Image').child('.png');
     await refImage.putFile(pickedImage!);
